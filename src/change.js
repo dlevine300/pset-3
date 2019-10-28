1f6ea046dddfab16d7e6bad02fdeea4b6d542eb9
David Levine <dlevine300@users.noreply.github.com>
2019-10-28 10:48:20 -0400

    test

--- src/change.js
+++ src/change.js
@@ -5,10 +5,22 @@ const MAX = 0.99;
 
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
+const Q =0.25;
+const D =0.10;
+const N =0.05;
+const P =0.01;
+
+const Tqcr = (MAX%Q);
+const Tdcr = (Tqcr%D);
+const Tncr = (Tdcr%N);
+const Tpcr = (Tncr%P);
+
 if (Number.isNaN(amount)) {
-    // print your error message here
+console.log('invalid'); // print your error message here
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+console.log('invalid');    // print your error message here
 } else {
     // write your code for making change here
 }
+
+console.log("d" + Q + D + N + P + "nnn")
