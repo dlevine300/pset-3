b92f7b582177ecab77a2f14e4216018c138c909c
David Levine <dlevine300@users.noreply.github.com>
2019-11-01 10:28:49 -0400

    i wanna commit #3

--- src/sign.js
+++ src/sign.js
@@ -2,14 +2,17 @@ const ReadlineSync = require("readline-sync");
 const Integer =ReadlineSync.question('\nEnter a Number: ');
 const Pos_number =(Integer>0);
 const Neg_number =(Integer<0);
+
 const MIN= Number.MIN_SAFE_INTEGER;
 const MAX= Number.MAX_SAFE_INTEGER;
+
 if (0>(Integer)) {
 console.log('\nnegative.');
 } else if (0 < Integer || Integer > 0) {
 console.log('\npositive.');
 } else {
 }
+
 if (Number.isNaN(Integer)) {
 console.log('\ninvalid.');
 } else if (Integer < MIN || Integer > MAX) {
