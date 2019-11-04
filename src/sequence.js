3e9cea9f4decb16b5bb5bf63ff2fc53f7136c1ba
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-11-04 17:12:57 -0500

    I wanna commit #4 (but its wrong)

---
+++ src/sequence.js
@@ -0,0 +1,14 @@
+const ReadlineSync = require("readline-sync");
+const Number1= ReadlineSync.question('Enter three numbers; ');
+const Number2= ReadlineSync.question();
+const Number3= ReadlineSync.question();
+
+if (Number3-Number2==2.2) {
+console.log('\nStrictly Increasing');
+
+} else if (Number.isNaN(Integer)) {
+console.log('\nInvalid.');
+
+} else {
+  console.log('\nOdd.');
+}
