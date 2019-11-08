cb9251c82a8375dff85da0c24366173b04876623
David Levine <55397183+dlevine300@users.noreply.github.com>
2019-11-07 22:09:04 -0500

    I wanna commit completed #4

--- src/sequence.js
+++ src/sequence.js
@@ -3,24 +3,30 @@ const Number1= ReadlineSync.question('\nEnter three numbers. \n');
 const Number2= ReadlineSync.question();
 const Number3= ReadlineSync.question();
 
-if ((Number3-Number2==2.2) || (Number2-Number1==2.2)) {
-console.log('\nStrictly Increasing.');
-
-} else if ((Number3-Number2>=0) || (Number2-Number1>=0)) {
- console.log('\nIncreasing.');
-
-} else ((Number3-Number2==0) || (Number2-Number1==0))
-  console.log('\nEqual.');
-
-if ((Number3-Number2<=0) || (Number2-Number1<=0)) {
- console.log('\nDecreasing.');
-
-}else if  ((Number3-Number2==(-2.2)||Number2-Number1==(-2.2))){
- console.log('\nStrictly Decreasing.');
-
-}else (1)
- console.log('\nUnordered.');
-
 if (Number.isNaN(Number1)) {
- console.log('\nInvalid.');
+    console.log("\nInvalid.");
+}
+
+else if (Number1 == Number2 || Number2 == Number3) {
+    console.log("\nEqual.");
+}
+
+else if (Number1 - Number2 == Number2 - Number3 || Number3 > Number2 || Number2 > Number1) {
+  console.log("\nStrictly increasing");
+}
+
+else if (Number1 < Number2 || Number2 < Number3) {
+  console.log("\nIncreasing.");
+}
+
+else if (Number3 - Number2 == Number2 - Number1) {
+  console.log("\nStrictly decreasing.");
+  }
+
+else if (Number1 > Number2 || Number2 > Number3) {
+  console.log("\nDecreasing");
+}
+
+else {
+ console.log("\nUnordered.");
 }
